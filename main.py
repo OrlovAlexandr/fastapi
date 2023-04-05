88bcd665a651f3786f65eff7686e60d1010c1d3a
OrlovAlexandr <eaglophone@gmail.com>
2023-04-05 17:13:02 +0500

    changed main.py comments test_main finish line added

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel  # pydantic занимается автоматической проверкой
+# формата и типа данных
 from transformers import pipeline
-from pydantic import BaseModel				# pydantic занимается автоматической проверкой формата и типа данных
-											# Импортировали вариант класса для моделей BaseModel 
-class Item(BaseModel):						# создаем на его основе свою модель
-    text: str 								# В параметре text будем передавать текст для определения тональности
 
-app = FastAPI()
+
+# Импортировали вариант класса для моделей BaseModel.
+# Не-а херь. Этот класс наследует pydantic, который занимается валидацией того,
+# что то, что подается на веб-серввер точно является строкой, или даже
+# преобразует в строку
+class Item(BaseModel):
+    text: str  # В параметре text будем передавать текст для определения тональности
+
+
+app = FastAPI()  # создаем на его основе свою модель
 classifier = pipeline("sentiment-analysis")
+
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-@app.post("/predict/")						# метод POST для больших текстов в теле запроса HTTP
-def predict(item: Item):					# в predict передается объект item типа Item
-    """Sentiment analysis for a text"""
-    return classifier(item.text )[0]		# классификатор вызывается для поля text объекта item
+
+@app.post("/predict/")  # метод POST для больших текстов в теле запроса HTTP
+def predict(item: Item):  # в predict передается объект item типа Item
+    return classifier(item.text)[0]  # классификатор вызывается для поля text
+                                     # объекта item
