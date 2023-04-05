8cd0ddf2464cf4bc1b495c1ed467ddd674551c51
OrlovAlexandr <eaglophone@gmail.com>
2023-04-05 17:33:47 +0500

    test_main.py predict functions added

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+
 from main import app
 
 client = TestClient(app)
@@ -8,3 +9,19 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_predict_positive():
+    response = client.post("/predict/",
+                           json={"text": "I like apples!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'POSITIVE'
+
+
+def test_predict_negative():
+    response = client.post("/predict/",
+                           json={"text": "I hate apples!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
